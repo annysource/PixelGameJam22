6ffbb2d6a0d797492bf5b387ff60f69fe78d88f4
annynaweb <sousa.annyc@gmail.com>
2022-05-07 01:20:49 -0300

    Forca do pulo config inicial

--- New Unity Project/Assets/Scripts/Platform.cs
+++ New Unity Project/Assets/Scripts/Platform.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if(rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
+        }
     }
 }
