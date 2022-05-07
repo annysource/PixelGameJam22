46938b6c1a3465933b99a20d4424b27017867cb1
annynaweb <sousa.annyc@gmail.com>
2022-05-07 19:35:11 -0300

    Pos Switch Build Web

---
+++ New Unity Project/Assets/Scripts/MusicLoop.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoop : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicStart;
+
+    void Start()
+    {
+        musicSource.PlayOneShot(musicStart);
+        musicSource.PlayScheduled(AudioSettings.dspTime + musicStart.length);
+    }
+}
