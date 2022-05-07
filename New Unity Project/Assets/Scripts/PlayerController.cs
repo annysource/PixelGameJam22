91d9dbbf01af72576a9e4f6bf424002fba83b01a
annynaweb <sousa.annyc@gmail.com>
2022-05-07 01:06:10 -0300

    experimento do pulo funcionando

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+
+    private float moveX;
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveX;
+        rb.velocity = velocity;
     }
 }
